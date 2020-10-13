2d68f9a1b5a593acd484236cf0e199c640cb1467
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-10-13 13:23:09 +0530

    import package

--- src/main.java
+++ src/main.java
@@ -1,2 +1,14 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: nandinee33
+ *   Date: 13/10/20
+ *   Time: 1:21 PM
+ */
+
 public class main {
+
+    public static void main(String[] args) {
+
+    }
+
 }
